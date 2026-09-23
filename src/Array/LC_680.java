8a752b17de3329744c8932948a1979fd053c9e51
sumankumarnayak <sumannayak192113114@gmail.com>
2026-09-23 20:59:27 +0530

    680

---
+++ src/Array/LC_680.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class LC_680 {
+    static void main() {
+        System.out.println(validPalindrome("sam9as"));
+    }
+    public static boolean validPalindrome(String s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return isPalindrome(l+1,r,s) || isPalindrome(l,r-1,s);
+            }
+            l++;r--;
+        }
+        return true;
+    }
+    public static boolean isPalindrome(int l, int r, String s){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;r--;
+        }
+        return true;
+    }
+}
