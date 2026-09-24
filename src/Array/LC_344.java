93dc41af0ca701136348ab5027ecb955b845f17b
sumankumarnayak <sumannayak192113114@gmail.com>
2026-09-24 22:52:46 +0530

    344

---
+++ src/Array/LC_344.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class LC_344 {
+    static void main() {
+        reverseString(new char[]{'h','e','l','l','o'});
+    }
+    public static void reverseString(char[] s) {
+        int l=0,r=s.length-1;
+        while(l<r){
+            char temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;r--;
+        }
+        for(char c:s){
+            System.out.println(c);
+        }
+    }
+}
